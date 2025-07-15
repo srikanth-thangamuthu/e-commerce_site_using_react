4f0ff97df5d1a81f0d0eab601415b679f2f9d93b
srikanth-thangamuthu <srikanth82146@gmail.com>
2025-07-15 20:43:35 +0530

    Add files via upload

---
+++ all_products.js
@@ -0,0 +1,643 @@
+import blackjacketmen from './black jacket.jpg'
+import brownjacketmen from './brown_sweater.jpg'
+import greenjacketmen from './olivegreen_sweater.jpg'
+import yellowjacketmen from './yellow_sweater.jpg'
+import navybluejacketmen from './navyblue_sweater.jpg'
+import whitejacketmen from './white_sweater.jpg'
+import redblackjacketmen from './redblack_sweater.jpg'
+import whitesjacketmen from './download (1).jpg'
+import brownsjacketmen from './download.jpg'
+import blackshirtmen from './black_colorshirt.jpg'
+import blackshirtuspamen from './black_uspashirt.jpg'
+import palepinkshirtmen from './palepink_shirt.jpg'
+import skyblueshirtmen from './skyblue_shirt.jpg'
+import whitetshirtmen from './white_shirt.jpg'
+import whiteshirtmen from './white_colorshirt.jpg'
+import whiteuspashirtmen from './white_uspashirt.jpg'
+import blackchinosmen from './black_chinos.jpg'
+import blackjoggersmen from './black_joggers.jpg'
+import blackformalsmen from './black_formal_pant.jpg'
+import olivegreenchinosmen from './olivegreen_chinos.jpg'
+import olivegreenjoggersmen from './olive green joggers.jpg'
+import whitesneakermen from './white_sneakers.jpg'
+import blackbootmen from'./black_boot.jpg'
+import brownbootmen from'./brown_boots.jpg'
+import blackkurtiwomen from './girl_blackkurti.jpg'
+import pinkkurtiwomen from './girl_pinkurta.jpg'
+import whitekurtiwomen from './girl_whitekurti.jpg'
+import yellowkurtiwomen from './girl_yellowkurti.jpg'
+import blackhoodiewomen from './girl_blackhoodie.jpg'
+import whitehoodiewomen from './girl_whitehoodie.jpg'
+import yellowhoodiewomen from './girl_yellowhoodie.jpg'
+import blacktshirtwomen from './girl_blacktshirt.jpg'
+// import browntshirtwomen from './girl_browntshirt.webp'
+import whiteshirtwomen from './girl_whiteshirt.jpg'
+// import blackcroptopwomen from './girl_blackcroptop.webp'
+// import whitecroptopwomen from './girl_whitecroptop.webp'
+import blackshortskirtwomen from './girl_blackshortskirt.jpg'
+// import whiteskirtwomen from './girl_whiteskirt.webp'
+import whitecheckedskirtwomen from './girl_whiteskirtchecked.jpg'
+import flowerskirtwomen from './girl_flowerskirt.jpg'
+import blackchinoswomen from './girl_blackchinos.jpg'
+import blackjoggerswomen from './girl_blackjoggers.jpg'
+import whitejoggersswomen from './girl_whitejoggers.jpg'
+import blackbootswomen from './girl_blackboots.jpg'
+import blacksneakersswomen from './girl_blacksneakers.jpg'
+import brownbootswomen from './girl_brownboots.jpg'
+import whitesneakersswomen from './girl_whitesneakers.jpg'
+import blackbootkids from './kid_blackboots.jpg'
+// import blackchinoskids from './kid_blackchinos.jpg'
+// import blackhoodiekids from './kid_blackhoodie.jpg'
+import blackjerkinkids from './kid_blackjerkin.jpg'
+// import blackjoggerskids from './kid_blackjoggers.jpg'
+// import blackshirtkids from './kid_blackshit.jpg'
+// import blacksneakerskids from './kid_blacksneakers.jpg'
+import blueshirtkids from './kid_blueshirt.jpg'
+// import brownbootskids from './kid_brownboots.webp'
+// import greenchinoskids from './kid_greenchinos.jpg'
+import greenjoggerskids from './kid_greenjoggers.jpg'
+import greenshirtkids from './kid_greenshirt.jpg'
+import bluejerkinkids from './kid_mildblue_jerkin.jpg'
+// import skybluehoodiekids from './kid_skybluehoodie.jpg'
+import skyblueshirtkids from './kid_skyblueshirt.jpg'
+import whiteshirtkids from './kid_white_shirt.jpg'
+// import whitehoodiekids from './kid_whitehoodie.jpg'
+// import whiteshirt2kids from './kid_white_shirt.webp'
+// import whitesneakerskids from './kid_whitesneakers.jpg'
+// import yellowhoodiekid from './kid_yellowhoodie.jpg'
+// import yellowshirtkid from './kid_yellowshirt.jpg'
+import kidtshirt from './kids_tshirt.jpg'
+import hero from './hand_image.webp'
+import logo from'./logo_rakstarz.jpg';
+let all_product=[
+    {
+        id:1,
+        name:"black jacket with zipper complete leather multipurpose for men",
+        category:"men",
+        image:blackjacketmen,
+        new_price:148.1,
+        old_price:179.3,
+    },
+    {
+        id:2,
+        name:"brown jacket with zipper complete leather multipurpose for men",
+        category:"men",
+        image:brownjacketmen,
+        new_price:151.1,
+        old_price:187.8,
+    },
+    {
+        id:3,
+        name:"green jacket with zipper complete leather multipurpose for men",
+        category:"men",
+        image:greenjacketmen,
+        new_price:147.4,
+        old_price:171.8,
+    },
+    {
+        id:4,
+        name:"yellow jacket with zipper complete leather multipurpose for men",
+        category:"men",
+        image:yellowjacketmen,
+        new_price:146.4,
+        old_price:166.8,
+    },
+    {
+        id:5,
+        name:"navyblue jacket with zipper complete leather multipurpose for men(premium)",
+        category:"men",
+        image:navybluejacketmen,
+        new_price:161.4,
+        old_price:201.8,
+    },
+     {
+        id:6,
+        name:"white jacket with zipper complete leather multipurpose for men(premium)",
+        category:"men",
+        image:whitejacketmen,
+        new_price:169.9,
+        old_price:209.8,
+    },
+    {
+        id:7,
+        name:"red with black jacket with zipper complete leather multipurpose for men(premium)",
+        category:"men",
+        image:redblackjacketmen,
+        new_price:160.7,
+        old_price:190.4,
+    },
+    {
+        id:8,
+        name:"dark brown jacket with zipper complete leather multipurpose for men(premium)",
+        category:"men",
+        image:whitesjacketmen,
+        new_price:180.7,
+        old_price:190.4,
+    },
+    {
+        id:9,
+        name:"ultra white jacket with zipper complete leather multipurpose for men(premium)",
+        category:"men",
+        image:brownsjacketmen,
+        new_price:199,
+        old_price:299,
+    },
+    {
+        id:10,
+        name:"black color polo neck tshirt from us polo assassin",
+        category:"men",
+        image:blackshirtmen,
+        new_price:71,
+        old_price:87,
+    },
+    {
+        id:11,
+        name:"black color shirt from us polo assassin",
+        category:"men",
+        image:blackshirtuspamen,
+        new_price:161,
+        old_price:200,
+    },
+   {
+        id:12,
+        name:"palepink color shirt from souled store",
+        category:"men",
+        image:palepinkshirtmen,
+            new_price:81,
+        old_price:87,
+    },
+    {
+        id:13,
+        name:"skyblue color shirt from souled store",
+        category:"men",
+        image:skyblueshirtmen,
+            new_price:80,
+        old_price:87,
+    },
+    {
+        id:14,
+        name:"white plain color shirt from souled store",
+        category:"men",
+        image:whiteshirtmen,
+            new_price:80,
+        old_price:87,
+    },
+    {
+        id:15,
+        name:"white color polo neck tshirt from us polo assassin",
+        category:"men",
+        image:whitetshirtmen,
+        new_price:77,
+        old_price:87,
+    },
+    {
+        id:16,
+        name:"white color shirt from us polo assassin",
+        category:"men",
+        image:whiteuspashirtmen,
+        new_price:169.3,
+        old_price:200,
+    },
+    {
+        id:17,
+        name:"black formal and casual chinos from max ",
+        category:"men",
+        image:blackchinosmen,
+        new_price:124.3,
+        old_price:200,
+    },
+     {
+        id:17,
+        name:"black formal pant from max ",
+        category:"men",
+        image:blackformalsmen,
+        new_price:89.3,
+        old_price:113,
+    },
+    {
+        id:18,
+        name:"black joggers pant for both cauals and semi-formals pant from max ",
+        category:"men",
+        image:blackjoggersmen,
+        new_price:91.3,
+        old_price:144,
+    },
+    {
+        id:19,
+        name:"olivegreen chinos pant for both cauals and semi-formals pant from max ",
+        category:"men",
+        image:olivegreenchinosmen,
+        new_price:87.4,
+        old_price:145,
+    },
+    {
+        id:20,
+        name:"olivegreen joggers pant for both cauals and semi-formals pant from max ",
+        category:"men",
+        image:olivegreenjoggersmen,
+        new_price:91.9,
+        old_price:122,
+    },
+     {
+        id:21,
+        name:"white sneakers from newzeakand",
+        category:"men",
+        image:whitesneakermen,
+        new_price:65.5,
+        old_price:119,
+    },
+    // {
+    //     id:22,
+    //     name:"black sneakers from newzeakand",
+    //     category:"men",
+    //     image:blacksneakersmen,
+    //     new_price:66.5,
+    //     old_price:115,
+    // },
+      {
+        id:22,
+        name:"black boots from router rabbit complete leater",
+        category:"men",
+        image:blackbootmen,
+        new_price:50.1,
+        old_price:109,
+    },
+    {
+        id:23,
+        name:"brown boots from router rabbit complete leather",
+        category:"men",
+        image:brownbootmen,
+        new_price:52.2,
+        old_price:109.5,
+    },
+    {
+        id:24,
+        name:"desi version of black kurti rotter brand",
+        category:"women",
+        image:blackkurtiwomen,
+        new_price:104.1,
+        old_price:129.6,
+    },
+    {
+        id:25,
+        name:"desi version of white kurti rotter brand",
+        category:"women",
+        image:whitekurtiwomen,
+        new_price:106.7,
+        old_price:129.6,
+    },
+    {
+        id:25,
+        name:"desi version of yellow kurti rotter brand",
+        category:"women",
+        image:yellowkurtiwomen,
+        new_price:97.6,
+        old_price:119.1,
+    },
+    {
+        id:26,
+        name:"desi version of pink kurti rotter brand",
+        category:"women",
+        image:pinkkurtiwomen,
+        new_price:96.6,
+        old_price:118.3,
+    },
+     {
+        id:27,
+        name:"black color hoodie for women from nike",
+        category:"women",
+        image:blackhoodiewomen,
+        new_price:119.7,
+        old_price:138.4,
+    },
+     {
+        id:28,
+        name:"white color hoodie for women from nike",
+        category:"women",
+        image:whitehoodiewomen,
+        new_price:120.6,
+        old_price:140.3,
+    },
+     {
+        id:29,
+        name:"yellow color hoodie for women from nike",
+        category:"women",
+        image:yellowhoodiewomen,
+        new_price:117.3,
+        old_price:139.1,
+    },
+    {
+        id:30,
+        name:"a us printed black tshirt",
+        category:"women",
+        image:blacktshirtwomen,
+        new_price:49.1,
+        old_price:57.3,
+    },
+    // {
+    //     id:31,
+    //     name:"brown plain tshirt with zipper from rabbitrotter",
+    //     category:"women",
+    //     image:browntshirtwomen,
+    //     new_price:71.2,
+    //     old_price:80.1,
+    // },
+     {
+        id:32,
+        name:"white shirt for women used for both formals and casuals from rabbitrotter",
+        category:"women",
+        image:whiteshirtwomen,
+        new_price:79.2,
+        old_price:84.9,
+    },
+    //  {
+    //     id:33,
+    //     name:"black croptop for women used for  casuals from rabbitrotter",
+    //     category:"women",
+    //     image:blackcroptopwomen,
+    //     new_price:71.4,
+    //     old_price:85.9,
+    // },
+    // {
+    //     id:34,
+    //     name:"white croptop for women used for  casuals from rabbitrotter",
+    //     category:"women",
+    //     image:whitecroptopwomen,
+    //     new_price:75.5,
+    //     old_price:85.9,
+    // },
+    {
+        id:35,
+        name:"black short skirt plain for women used for  casuals from rabbitrotter",
+        category:"women",
+        image:blackshortskirtwomen,
+        new_price:41.6,
+        old_price:53.6,
+    },
+    // {
+    //     id:36,
+    //     name:"white short skirt plain for women used for  casuals from rabbitrotter",
+    //     category:"women",
+    //     image:whiteskirtwomen,
+    //     new_price:43.7,
+    //     old_price:53.6,
+    // },
+    {
+        id:37,
+        name:"white short skirt checked for women used for  casuals from rabbitrotter",
+        category:"women",
+        image:whitecheckedskirtwomen,
+        new_price:47.4,
+        old_price:57.1,
+    },
+    {
+        id:38,
+        name:"white short skirt checked for women used for  casuals from rabbitrotter",
+        category:"women",
+        image:whitecheckedskirtwomen,
+        new_price:47.4,
+        old_price:57.1,
+    },
+    {
+        id:39,
+        name:"white short skirt flower printed for women used for  casuals from rabbitrotter",
+        category:"women",
+        image:flowerskirtwomen,
+        new_price:51.8,
+        old_price:61.6,
+    },
+    {
+        id:40,
+        name:"black chinos used for both casual and formals from nike",
+        category:"women",
+        image:blackchinoswomen,
+        new_price:99.9,
+        old_price:119.9,
+    },
+    {
+        id:41,
+        name:"black joggers used for both casual and formals from nike",
+        category:"women",
+        image:blackjoggerswomen,
+        new_price:97.9,
+        old_price:117.9,
+    },
+    {
+        id:41,
+        name:"white joggers used for both casual and formals from nike",
+        category:"women",
+        image:whitejoggersswomen,
+        new_price:103.9,
+        old_price:129.9,
+    },
+    {
+        id:42,
+        name:"black boot for both casual and formal use from lacutose(complete leather)",
+        category:"women",
+        image:blackbootswomen,
+        new_price:199.9,
+        old_price:231.4,
+    },
+    {
+        id:43,
+        name:"brown boot for both casual and formal use from lacutose(complete leather)",
+        category:"women",
+        image:brownbootswomen,
+        new_price:193.4,
+        old_price:229.4,
+    },
+    {
+        id:44,
+        name:"black sneakers for women from nike",
+        category:"women",
+        image:blacksneakersswomen,
+        new_price:121.6,
+        old_price:156.3,
+    },
+    {
+        id:45,
+        name:"white sneakers for women from nike",
+        category:"women",
+        image:whitesneakersswomen,
+        new_price:141.6,
+        old_price:176.3,
+    },
+     {
+        id:46,
+        name:"black boot for both casual and formal use from lacutose(complete leather)",
+        category:"kids",
+        image:blackbootkids,
+        new_price:166.6,
+        old_price:199.9,
+    },
+    // {
+    //     id:47,
+    //     name:"black chinos for kids from nike",
+    //     category:"kids",
+    //     image:blackchinoskids,
+    //     new_price:123.6,
+    //     old_price:150.7,
+    // },
+    // {
+    //     id:47,
+    //     name:"plain black hoodie without prints from rabbitrotter",
+    //     category:"kids",
+    //     image:blackhoodiekids,
+    //     new_price:51.6,
+    //     old_price:66.7,
+    // },
+    {
+        id:48,
+        name:"black jerkin multipurpose use from lacutose(complete leather)",
+        category:"kids",
+        image:blackjerkinkids,
+        new_price:142.6,
+        old_price:199.9,
+    },
+    // {
+    //     id:49,
+    //     name:"black joggers for kids from nike",
+    //     category:"kids",
+    //     image:blackjoggerskids,
+    //     new_price:122.6,
+    //     old_price:150.7,
+    // },
+    // {
+    //     id:50,
+    //     name:"black shirt for kids from rabbitrotter",
+    //     category:"kids",
+    //     image:blackshirtkids,
+    //     new_price:43.7,
+    //     old_price:50.6,
+    // },
+    // {
+    //     id:51,
+    //     name:"blacksneakers for kids from nike",
+    //     category:"kids",
+    //     image:blacksneakerskids,
+    //     new_price:70.6,
+    //     old_price:80.00,
+    // },
+    {
+        id:52,
+        name:"blue shirt for kids from rabbitrotter",
+        category:"kids",
+        image:blueshirtkids,
+        new_price:41.4,
+        old_price:50.6,
+    },
+    // {
+    //     id:53,
+    //     name:"brown boot for both casual and formal use from lacutose(complete leather)",
+    //     category:"kids",
+    //     image:brownbootskids,
+    //     new_price:164.6,
+    //     old_price:194.9,
+    // },
+    //  {
+    //     id:54,
+    //     name:"green chinos for kids from nike",
+    //     category:"kids",
+    //     image:greenchinoskids,
+    //     new_price:123.6,
+    //     old_price:150.7,
+    // },
+    // {
+    //     id:55,
+    //     name:"green joggers for kids from nike",
+    //     category:"kids",
+    //     image:greenjoggerskids,
+    //     new_price:117.4,
+    //     old_price:150.7,
+    // },
+    {
+        id:56,
+        name:"green shirt for kids from rabbitrotter",
+        category:"kids",
+        image:greenshirtkids,
+        new_price:51.8,
+        old_price:70.1,
+    },
+    {
+        id:57,
+        name:"mild blue jerkin multiputpose for kids from lactuose(complete leather)",
+        category:"kids",
+        image:bluejerkinkids,
+        new_price:139.1,
+        old_price:170.6,
+    },
+    // {
+    //     id:58,
+    //     name:"skyblue hoodie from nike for kids",
+    //     category:"kids",
+    //     image:skybluehoodiekids,
+    //     new_price:119.1,
+    //     old_price:140.8,
+    // },
+    {
+        id:59,
+        name:"skyblue shirt for kids from rabbitrotter",
+        category:"kids",
+        image:skyblueshirtkids,
+        new_price:53.7,
+        old_price:70.1,
+    },
+    {
+        id:60,
+        name:"white shirt for kids from rabbitrotter",
+        category:"kids",
+        image:whiteshirtkids,
+        new_price:57.7,
+        old_price:72.1,
+    },
+    // {
+    //     id:61,
+    //     name:"exclusive white hoodie from nike for kids(premuim limited edition)",
+    //     category:"kids",
+    //     image:whitehoodiekids,
+    //     new_price:210.0,
+    //     old_price:400.0,
+    // },
+    // {
+    //     id:62,
+    //     name:"yellow shirt for kids from rabbitrotter",
+    //     category:"kids",
+    //     image:yellowshirtkid,
+    //     new_price:51.7,
+    //     old_price:72.1,
+    // },
+    // {
+    //     id:63,
+    //     name:"white sneakers for kids from nike",
+    //     category:"kids",
+    //     image:whitesneakerskids,
+    //     new_price:99.9,
+    //     old_price:120.6,
+    // },
+    // {
+    //     id:63,
+    //     name:"yellowhoodie  for kids from nike",
+    //     category:"kids",
+    //     image:yellowhoodiekid,
+    //     new_price:89.7,
+    //     old_price:119.3,
+    // },
+    {
+        id:64,
+        name:"black plain tshirt for kids from rabbit rotter",
+        category:"kids",
+        image:kidtshirt,
+        new_price:37,
+        old_price:43
+    }
+
+]
+export default all_product;
+
+
+
+
+
+
+
+
---
+++ data.js
@@ -0,0 +1,41 @@
+import blackjacketmen from './black jacket.jpg'
+import whitehoodiekids from './kid_whitehoodie.jpg'
+import whitejacketmen from './white_sweater.jpg'
+import blackbootkids from './kid_blackboots.jpg'
+import blackkurtiwomen from './girl_blackkurti.jpg'
+let data_products=[
+    {
+        id:1,
+        name:"black jacket with zipper complete leather multipurpose for men",
+        category:"men",
+        image:blackjacketmen,
+        new_price:148.1,
+        old_price:179.3,
+    },
+    
+     {
+        id:6,
+        name:"white jacket with zipper complete leather multipurpose for men(premium)",
+        category:"men",
+        image:whitejacketmen,
+        new_price:169.9,
+        old_price:209.8,
+    },
+     {
+        id:46,
+        name:"black boot for both casual and formal use from lacutose(complete leather)",
+        category:"kids",
+        image:blackbootkids,
+        new_price:166.6,
+        old_price:199.9,
+    },
+     {
+        id:24,
+        name:"desi version of black kurti rotter brand most wanted one in girls category",
+        category:"women",
+        image:blackkurtiwomen,
+        new_price:104.1,
+        old_price:129.6,
+    },
+]
+export default data_products
